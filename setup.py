4363ea6a50df0e9c2bdd86248d8b21ae782f4c27
Asher Foa <asher@toolchain.com>
2020-03-09 14:33:47 -0700

    bump to v0.3.0 to avoid confusion w/ test pypi. (#5)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ PROJECT_URLS = {
 
 setup(
     name="flake8-os-walk",
-    version="0.1.0",
+    version="0.3.0",
     description="Flake8 plugin which usage of os.walk().",
     author="Asher Foa",
     author_email="asher@asherfoa.com",
