242621e8b5addcf694114cb69ae8212c93008a13
Asher Foa <asher@toolchain.com>
2020-03-09 14:29:43 -0700

    Add metadata to setup.py (#4)

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
+with open("README.md") as fl:
+    LONG_DESCRIPTION = fl.read()
+
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/asherf/flake8-os-walk/issues",
+    "Source Code": "https://github.com/asherf/flake8-os-walk",
+}
 
 setup(
     name="flake8-os-walk",
@@ -9,6 +16,9 @@ setup(
     author="Asher Foa",
     author_email="asher@asherfoa.com",
     url="https://github.com/asherf/flake8-os-walk",
+    long_description=LONG_DESCRIPTION,
+    project_urls=PROJECT_URLS,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     keywords="flake8",
     entry_points={"flake8.extension": ["OW = flake8_os_walk:OSWalkChecker"]},
